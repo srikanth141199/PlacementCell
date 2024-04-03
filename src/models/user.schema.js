65ed5bcaeb13b854856719bdcf02ab8de5a532a9
Srikanth K <kolleparasrikanth@gmail.com>
2024-04-03 22:37:46 +0530

    SignInn, SignOut display implemented

---
+++ src/models/user.schema.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+export const userScheme = new mongoose.Schema({
+    name: {type: String, maxLength:[25, "Name can not be more than 25 characters"]},
+    email: {type: String, unique: true, required: true,
+        match:[/.+\@.+\../,"Please enter a valid email"]//regular expressions as validators
+    },
+    password: {type: String,
+        validate:{
+            validator: function(value){
+                return /^(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,12}$/.test(value);
+            },
+            message:"Password should be between 8-12 characters and a special character"
+        }
+    },
+})
