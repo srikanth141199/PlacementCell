f12edcd6f571bff1b4435694c0a64a313a03eb1b
Srikanth K <kolleparasrikanth@gmail.com>
2024-04-13 22:29:31 +0530

    fixed download report

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -72,60 +72,6 @@ export const createUser = async function (req, res) {
 	}
 };
 
-// download report
-// export const downloadCsv = async function (req, res) {
-// 	try {
-// 		const students = await Student.find({});
-
-// 		let data = '';
-// 		let no = 1;
-// 		let csv = 'S.No, Name, Email, College, Placemnt, Contact Number, Batch, DSA Score, WebDev Score, React Score, Interview, Date, Result';
-
-// 		for (let student of students) {
-// 			data =
-// 				no +
-// 				',' +
-// 				student.name +
-// 				',' +
-// 				student.email +
-// 				',' +
-// 				student.college +
-// 				',' +
-// 				student.placement +
-// 				',' +
-// 				student.contactNumber +
-// 				',' +
-// 				student.batch +
-// 				',' +
-// 				student.dsa +
-// 				',' +
-// 				student.webd +
-// 				',' +
-// 				student.react;
-
-// 			if (student.interviews.length > 0) {
-// 				for (let interview of student.interviews) {
-// 					data += ',' + interview.company + ',' + interview.date.toString() + ',' + interview.result;
-// 				}
-// 			}
-// 			no++;
-// 			csv += '\n' + data;
-// 		}
-
-// 		const dataFile = fs.writeFile('src/report/data.csv', csv, function (error, data) {
-// 			if (error) {
-// 				console.log(error);
-// 				return res.redirect('back');
-// 			}
-// 			console.log('Report generated successfully');
-// 			return res.download('report/data.csv');
-// 		});
-// 	} catch (error) {
-// 		console.log(`Error in downloading file: ${error}`);
-// 		return res.redirect('back');
-// 	}
-// };
-
 export const downloadCsv = async function (req, res) {
     try {
         const students = await Student.find({});
